137b9158fc0c4a57dfeeb936f94b3645be6bf540
zsoltMeszaros <meszazaolt13@gmail.com>
2019-08-21 14:19:16 +0200

    70 óra ment el erre a DB-re

--- sample_data/pro-man-69.sql
+++ sample_data/pro-man-69.sql
@@ -15,10 +15,26 @@ CREATE TABLE cards (
     id   SERIAL PRIMARY KEY NOT NULL,
     board_id INTEGER NOT NULL,
     title VARCHAR(15) NOT NULL,
-    status VARCHAR(15) NOT NULL
+    status VARCHAR(15) NOT NULL DEFAULT 0,
+    card_order INTEGER
 );
 
 
 ALTER TABLE ONLY cards
     ADD CONSTRAINT fk_board_id FOREIGN KEY (board_id) REFERENCES boards(id);
 
+INSERT INTO boards VALUES (1,'Board 1');
+INSERT INTO boards VALUES (2,'BÓRD 2');
+
+INSERT INTO cards VALUES (1,1,'new card 1',0,0);
+INSERT INTO cards VALUES (2,1,'new card 2',0,1);
+INSERT INTO cards VALUES (3,1,'new card 3',1,0);
+INSERT INTO cards VALUES (4,1,'new card 4',2,0);
+INSERT INTO cards VALUES (5,2,'new card 5',3,0);
+INSERT INTO cards VALUES (6,2,'new card 6',3,1);
+INSERT INTO cards VALUES (7,1,'new card 1',0,0);
+INSERT INTO cards VALUES (8,1,'new card 1',0,1);
+INSERT INTO cards VALUES (9,1,'new card 1',1,0);
+INSERT INTO cards VALUES (10,1,'new card 1',2,0);
+INSERT INTO cards VALUES (11,1,'new card 1',3,0);
+INSERT INTO cards VALUES (12,1,'new card 1',3,1);
