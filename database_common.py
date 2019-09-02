f926fdfa6436df12cfbf332cd3f476f82f8994a1
Sándor Negyeliczky <sandor.negyeliczky@gmail.com>
2019-09-02 15:30:22 +0200

    fix Heroku db bug

--- database_common.py
+++ database_common.py
@@ -39,8 +39,8 @@ def get_connection_string():
 
 def open_database():
     try:
-        connection_string = get_heroku_connection()  # original: get_connection_string()
-        connection = psycopg2.connect(connection_string)
+        # connection_string = get_connection_string()
+        connection = get_heroku_connection()  # psycopg2.connect(conection_string)
         connection.autocommit = True
     except psycopg2.DatabaseError as exception:
         print('Database connection problem')
